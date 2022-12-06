872e1fe502e90f657497dab7899cca5f85992c19
Danil M <malaxoff.dan@gmail.com>
2022-12-07 01:50:54 +0700

    Реализован блокчейн на Golang

---
+++ block.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
+}
+
+// Cериализация структуры Block в массив []byte
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+// Десереализация массива []byte в структуру Block
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
+}
+
+// Конструктор для нового блока
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
+
+// Создание начального блока блокчейна
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module block_chain-module
+
+go 1.19
+
+require github.com/boltdb/bolt v1.3.1
+
+require golang.org/x/sys v0.3.0 // indirect
